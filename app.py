b416eb4d2b3757314776b11a42cb057ec4194ce0
anugerah bakti prasisto <95016108+LOLIZIETA@users.noreply.github.com>
2025-05-30 10:17:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from tensorflow.keras.preprocessing import image
 # Fungsi untuk load model
 @st.cache_resource
 def load_garbage_model():
-    model = load_model('model_classification_sampah.h5')
+    model = load_model('model_garbage_classification_28_Mei.h5')
     return model
 
 model = load_garbage_model()
